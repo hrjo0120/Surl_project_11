d1790362c4795ec86cfd618209fae58a4ac71f49
hrjo0120 <hrjo0120@gmail.com>
2024-11-25 14:57:49 +0900

    매 요청마다 파라미터 정보로 인증

--- src/main/java/com/koreait/surl_project_11/global/rq/Rq.java
+++ src/main/java/com/koreait/surl_project_11/global/rq/Rq.java
@@ -4,7 +4,9 @@ import com.koreait.surl_project_11.domain.member.member.entity.Member;
 import com.koreait.surl_project_11.domain.member.member.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
@@ -17,9 +19,18 @@ public class Rq {
 
     private final MemberService memberService;
 
-    public Member getMember() {
-        return memberService.getReferenceById(1L);
-    }
+    @Getter
+    @Setter
+    private Member member;
+
+//    public Member getMember() {
+//        // 개선해야할 부분
+//        // 이 부분 때문에 계속 3번으로 요청이 가지만, 요청할 때마다 인증정보나 클라이언트가 누군지 분별할 수 있게 같이 줘야함
+//        // 매 요청마다 인증정보를 같이 보내야함
+//
+//        // 필요한 이유? 내가 만들어 둔 서비스는 하나이지만, 사용자는 다수이기 때문에
+//        return memberService.getReferenceById(3L);  // user1
+//    }
 
     public String getCurrentUrlPath() {
         return req.getRequestURI();

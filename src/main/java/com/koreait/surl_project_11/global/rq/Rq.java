ca1dfbebf620476c577704e2b0cbcd78e6c44919
hrjo0120 <hrjo0120@gmail.com>
2024-11-25 15:54:20 +0900

    getMember에 캐시 로직 추가

--- src/main/java/com/koreait/surl_project_11/global/rq/Rq.java
+++ src/main/java/com/koreait/surl_project_11/global/rq/Rq.java
@@ -16,19 +16,27 @@ import org.springframework.web.context.annotation.RequestScope;
 public class Rq {
     private final HttpServletRequest req;
     private final HttpServletResponse resp;
-
     private final MemberService memberService;
+
+    private Member member;
 
 //    @Getter
 //    @Setter
 //    private Member member;
 
     public Member getMember() {
+        if (member != null) return member;   // 캐시 데이터 방식, 메모리 캐싱
+
         String actorUsername = req.getParameter("actorUsername");
+        String actorPassword = req.getParameter("actorPassword");
 
-        if (Ut.str.isBlank(actorUsername)) throw new GlobalException("401-1", "인증정보 입력하세요");
+        if (Ut.str.isBlank(actorUsername)) throw new GlobalException("401-1", "인증정보(아이디)를 입력해주세요");
+        if (Ut.str.isBlank(actorPassword)) throw new GlobalException("401-2", "인증정보(비밀번호)를 입력해주세요");
 
-        Member loginedMember = memberService.findByUsername(actorUsername).orElseThrow(() -> new GlobalException("401-2", "인증 정보가 올바르지 않습니다"));
+        Member loginedMember = memberService.findByUsername(actorUsername).orElseThrow(() -> new GlobalException("403-3", "해당 회원은 없습니다"));
+        if (!loginedMember.getPassword().equals(actorPassword)) throw new GlobalException("403-4", "비밀번호가 틀립니다");
+
+        member = loginedMember;
 
         return loginedMember;
     }

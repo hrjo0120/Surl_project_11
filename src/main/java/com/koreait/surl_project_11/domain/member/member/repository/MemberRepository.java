53d0de8cd90280aea4008f63e34cca323d05a924
hrjo0120 <hrjo0120@gmail.com>
2024-11-27 15:37:48 +0900

    apiKey 도입

--- src/main/java/com/koreait/surl_project_11/domain/member/member/repository/MemberRepository.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/repository/MemberRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);   // 조회 결과가 최대 1개. 따라서 Optional 을 써줌
+
+    Optional<Member> findByApiKey(String apiKey);
 }
--- src/main/java/com/koreait/surl_project_11/domain/member/member/service/MemberService.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/service/MemberService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -46,6 +47,7 @@ public class MemberService {
                 .username(username)
                 .password(passwordEncoder.encode(password))
                 .nickname(nickname)
+                .apiKey(UUID.randomUUID().toString()) // 유니크한 코드를 생성
                 .build();
         memberRepository.save(member);
         return RsData.of("회원가입이 완료되었습니다.", member);
@@ -65,5 +67,9 @@ public class MemberService {
 
     public Optional<Member> findById(long id) {
         return memberRepository.findById(id);
+    }
+
+    public Optional<Member> findByApiKey(String apiKey) {
+        return memberRepository.findByApiKey(apiKey);
     }
 }

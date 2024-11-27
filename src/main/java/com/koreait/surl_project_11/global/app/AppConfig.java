fa66958cc42d91dc5d3aefe29e831fe160b20915
hrjo0120 <hrjo0120@gmail.com>
2024-11-27 14:06:30 +0900

    자격없는 접근에 대한 오류

--- src/main/java/com/koreait/surl_project_11/global/app/AppConfig.java
+++ src/main/java/com/koreait/surl_project_11/global/app/AppConfig.java
@@ -1,5 +1,8 @@
 package com.koreait.surl_project_11.global.app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -7,6 +10,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
+
+    @Getter
+    public static ObjectMapper objectMapper;
+
+    @Autowired  // 잭슨의 Bean을 받아오는 기능
+    public void setObjectMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     // 비빌번호 암호화
     // SecurityConfig에서 만들어도 문제되지 않음
     // 보안 관련된 bean을 등록하고 app에 관련된 bean을 등록한다
--- src/main/java/com/koreait/surl_project_11/standard/util/Ut.java
+++ src/main/java/com/koreait/surl_project_11/standard/util/Ut.java
@@ -1,5 +1,8 @@
 package com.koreait.surl_project_11.standard.util;
 
+import com.koreait.surl_project_11.global.app.AppConfig;
+import lombok.SneakyThrows;
+
 public class Ut {
     public static class str {
         public static boolean isBlank(String str) {
@@ -11,4 +14,12 @@ public class Ut {
             return !isBlank(str);
         }
     }
+
+    public static class json {
+        @SneakyThrows
+        public static String toString(Object obj) {    // 스프링부트에 등록되어있는 bean을 내가 활용할 수 있는 형태로 바꿀거야
+            return AppConfig.getObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        }
+    }
 }
+
